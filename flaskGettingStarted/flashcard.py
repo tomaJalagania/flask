259d60379f86cfd025582f67b9f110f5b3d86302
toma jalagania <toma.jalagania@gmail.com>
2026-04-29 15:46:27 +0400

    add /date route

--- flaskGettingStarted/flashcard.py
+++ flaskGettingStarted/flashcard.py
@@ -1,8 +1,12 @@
 from  flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
 # create main router
 @app.route("/")
 def welcome():
     return "Welocm to my flashkard application"
+
+@app.route("/date")
+def date():
+    return "this server return "+str(datetime.now())
