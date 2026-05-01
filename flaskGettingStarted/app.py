7de70a9f32344536dce1c5b83cd507e0d7632d91
toma jalagania <toma.jalagania@gmail.com>
2026-05-01 16:43:27 +0400

    for loop

--- flaskGettingStarted/app.py
+++ flaskGettingStarted/app.py
@@ -7,8 +7,10 @@ card = db
 # create main router
 @app.route("/")
 def welcome():
+    cards = db
     return render_template("welcome.html",
-                           messages = "this is message"
+                           messages = "this is message",
+                           cards = cards
                            )
 
 @app.route("/card/<int:index>")
@@ -16,9 +18,13 @@ def flash_card(index):
     
     try:
         card = db[index]
+        max_lengt = len(db)
+        print(max_lengt)
         return render_template("card.html",
                            card = card,
-                           index = index )
+                           index = index,
+                           max_lengt = max_lengt - 1
+                             )
     except IndexError:
         abort(404)
     
